74767862c95312ddfabfc713d0859a78b9d92cc0
TAeng <T4eng.trirong@gmail.com>
2018-05-13 05:46:27 +0700

    [+] environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,10 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  API_BASE_URL: 'https://api-medium-shared.herokuapp.com',
+  API_SECRET: {
+    clientId: '704670e41f3',
+    clientSecret: 'aa0147624e82797a3b47d1c157c053628e89cc21',
+  },
+  redirectUri: 'http://127.0.0.1:4200',
+  Host: 'https://api.medium.com/v1',
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
