ee7c92f19401cc4184860e593fced0bf09bb1863
TAeng <T4eng.trirong@gmail.com>
2018-05-13 17:30:01 +0700

    [modify] code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { environment } from '../environments/environment'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,10 +16,7 @@ export class AppComponent implements OnInit {
   Url = 'https://medium.com/m/oauth/authorize?' + 'client_id=' + environment.API_SECRET.clientId + '&scope=' + this.scope + '&state=state&response_type=code&redirect_uri=' + environment.redirectUri;
 
   ngOnInit(){
-    var url = window.location.href.slice().toString(); 
-    var CVurl = new URL(url);
-    var code = CVurl.searchParams.get("code");
-    console.log(code);
+    
   }
  
 }
