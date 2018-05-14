7dff5b2de40665cbd959d66d3db6d08acf6e2b38
TAeng <T4eng.trirong@gmail.com>
2018-05-15 02:45:26 +0700

    [modify] path environtment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   API_BASE_URL: 'https://api-medium-shared.herokuapp.com',
   clientId: '704670e41f3',
   clientSecret: 'aa0147624e82797a3b47d1c157c053628e89cc21',
-  redirectUri: 'http://127.0.0.1:4200/callback',
+  redirectUri: 'https://t4eng.github.io/Angular-OAuth2-API-Medium/callback',
   Host: 'https://api.medium.com/v1',
   scope: 'basicProfile,listPublications',
   state: 'state',
   responseType: 'code',
-  GET_TEMP_CODE: 'https://medium.com/m/oauth/authorize?client_id=704670e41f3&scope=basicProfile,listPublications&state=state&response_type=code&redirect_uri=http://127.0.0.1:4200/callback'
+  GET_TEMP_CODE: 'https://medium.com/m/oauth/authorize?client_id=704670e41f3&scope=basicProfile,listPublications&state=state&response_type=code&redirect_uri=https://t4eng.github.io/Angular-OAuth2-API-Medium/callback'
 }
